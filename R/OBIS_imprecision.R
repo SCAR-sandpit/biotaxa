da63e4aa126d9e77b6c27285d33b20ddef8a9f7d
Hsun-Yi Hsieh <datascience.biodiversity.aq@gmail.com>
2017-11-20 13:24:10 +0100

    OBIS_imprecisionrate

---
+++ R/OBIS_imprecision.R
@@ -0,0 +1,16 @@
+#'Return the frequency ranking of an interested lower taxon based on a higher taxon
+#'
+#' @param taxa A string.
+#' @return the percentage of taxa occurrences on OBIS
+#' @examples
+#'\dontrun{
+#'OBIS_imprecisionrate("Abra")
+#'}
+#'@export
+
+OBIS_imprecisionrate <- function(taxa) {
+  dd <- occurrence(taxa)$species
+  incomplete <- length(which(is.na(dd == TRUE)))
+  imprecisionrate <- incomplete / length(dd)
+  return(imprecisionrate)
+}
